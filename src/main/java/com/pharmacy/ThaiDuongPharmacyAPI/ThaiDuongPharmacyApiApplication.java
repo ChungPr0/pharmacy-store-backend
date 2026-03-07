e9e953fd2bf1262d8b1259fef192faac13835d68
TIEN CHUNG <tienchung310725@gmail.com>
2026-03-07 22:24:33 +0700

    Khoi tao project Spring Boot

---
+++ src/main/java/com/pharmacy/ThaiDuongPharmacyAPI/ThaiDuongPharmacyApiApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.ThaiDuongPharmacyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaiDuongPharmacyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaiDuongPharmacyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmacy/ThaiDuongPharmacyAPI/ThaiDuongPharmacyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacy.ThaiDuongPharmacyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaiDuongPharmacyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
